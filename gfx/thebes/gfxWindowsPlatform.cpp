b07345e526a1afc3decb44cd929747756c68f9fe
K4sum1 <k4sum1@eclipse.cx>
2024-02-26 15:40:34 -0800

    Revert "Bug 1845746 - Remove gfxWindowsPlatform::DwmCompositionEnabled. r=gfx-reviewers,jrmuizel"
    This reverts commit 4bf50bac40c0bbfcde401e892abef3606969eb97.

--- gfx/thebes/gfxWindowsPlatform.cpp
+++ gfx/thebes/gfxWindowsPlatform.cpp
@@ -259,7 +259,9 @@ class D3DSharedTexturesReporter final : public nsIMemoryReporter {
 NS_IMPL_ISUPPORTS(D3DSharedTexturesReporter, nsIMemoryReporter)
 
 gfxWindowsPlatform::gfxWindowsPlatform()
-    : mRenderMode(RENDER_GDI), mSupportsHDR(false) {
+    : mRenderMode(RENDER_GDI),
+      mSupportsHDR(false),
+      mDwmCompositionStatus(DwmCompositionStatus::Unknown) {
   // If win32k is locked down then we can't use COM STA and shouldn't need it.
   // Also, we won't be using any GPU memory in this process.
   if (!IsWin32kLockedDown()) {
@@ -396,6 +398,27 @@ void gfxWindowsPlatform::InitAcceleration() {
   Factory::SetSystemTextQuality(gfxVars::SystemTextQuality());
   gfxVars::SetSystemTextQualityListener(
       gfxDWriteFont::SystemTextQualityChanged);
+
+  if (XRE_IsParentProcess()) {
+    BOOL dwmEnabled = FALSE;
+    if (FAILED(::DwmIsCompositionEnabled(&dwmEnabled)) || !dwmEnabled) {
+      gfxVars::SetDwmCompositionEnabled(false);
+    } else {
+      gfxVars::SetDwmCompositionEnabled(true);
+    }
+  }
+
+  // gfxVars are not atomic, but multiple threads can query DWM status
+  // Therefore, mirror value into an atomic
+  mDwmCompositionStatus = gfxVars::DwmCompositionEnabled()
+                              ? DwmCompositionStatus::Enabled
+                              : DwmCompositionStatus::Disabled;
+
+  gfxVars::SetDwmCompositionEnabledListener([this] {
+    this->mDwmCompositionStatus = gfxVars::DwmCompositionEnabled()
+                                      ? DwmCompositionStatus::Enabled
+                                      : DwmCompositionStatus::Disabled;
+  });
 
   // CanUseHardwareVideoDecoding depends on DeviceManagerDx state,
   // so update the cached value now.
@@ -1469,6 +1492,12 @@ void gfxWindowsPlatform::InitGPUProcessSupport() {
   // If we're still enabled at this point, the user set the force-enabled pref.
 }
 
+bool gfxWindowsPlatform::DwmCompositionEnabled() {
+  MOZ_RELEASE_ASSERT(mDwmCompositionStatus != DwmCompositionStatus::Unknown);
+
+  return mDwmCompositionStatus == DwmCompositionStatus::Enabled;
+}
+
 class D3DVsyncSource final : public VsyncSource {
  public:
   D3DVsyncSource()
@@ -1482,6 +1511,11 @@ class D3DVsyncSource final : public VsyncSource {
   }
 
   void SetVsyncRate() {
+    if (!gfxWindowsPlatform::GetPlatform()->DwmCompositionEnabled()) {
+      mVsyncRate = TimeDuration::FromMilliseconds(1000.0 / 60.0);
+      return;
+    }
+
     DWM_TIMING_INFO vblankTime;
     // Make sure to init the cbSize, otherwise GetCompositionTiming will fail
     vblankTime.cbSize = sizeof(DWM_TIMING_INFO);
@@ -1629,6 +1663,15 @@ class D3DVsyncSource final : public VsyncSource {
       MOZ_ASSERT(vsync <= TimeStamp::Now());
       NotifyVsync(vsync, vsync + mVsyncRate);
 
+      // DwmComposition can be dynamically enabled/disabled
+      // so we have to check every time that it's available.
+      // When it is unavailable, we fallback to software but will try
+      // to get back to dwm rendering once it's re-enabled
+      if (!gfxWindowsPlatform::GetPlatform()->DwmCompositionEnabled()) {
+        ScheduleSoftwareVsync(vsync);
+        return;
+      }
+
       HRESULT hr = E_FAIL;
       if (!StaticPrefs::gfx_vsync_force_disable_waitforvblank()) {
         UpdateVBlankOutput();
@@ -1732,6 +1775,11 @@ class D3DVsyncSource final : public VsyncSource {
 already_AddRefed<mozilla::gfx::VsyncSource>
 gfxWindowsPlatform::CreateGlobalHardwareVsyncSource() {
   MOZ_RELEASE_ASSERT(NS_IsMainThread(), "GFX: Not in main thread.");
+
+  if (!DwmCompositionEnabled()) {
+    NS_WARNING("DWM not enabled, falling back to software vsync");
+    return GetSoftwareVsyncSource();
+  }
 
   RefPtr<VsyncSource> d3dVsyncSource = new D3DVsyncSource();
   return d3dVsyncSource.forget();
