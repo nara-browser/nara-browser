c68174cda565fdb7f95b2a530dfd30d8b667b952
K4sum1 <k4sum1@eclipse.cx>
2024-02-27 23:25:57 -0800

    Revert "Bug 1843230 - Remove IsWin8OrLater checks from dom/geolocation/ r=emk"
    This reverts commit 1fc1316f1a8b7bcfa97b4dcc7bd5ed51afd94940.

--- dom/geolocation/Geolocation.cpp
+++ dom/geolocation/Geolocation.cpp
@@ -56,6 +56,7 @@ class nsIPrincipal;
 
 #ifdef XP_WIN
 #  include "WindowsLocationProvider.h"
+#  include "mozilla/WindowsVersion.h"
 #endif
 
 // Some limit to the number of get or watch geolocation requests
@@ -579,7 +580,8 @@ nsresult nsGeolocationService::Init() {
 #endif
 
 #ifdef XP_WIN
-  if (Preferences::GetBool("geo.provider.ms-windows-location", false)) {
+  if (Preferences::GetBool("geo.provider.ms-windows-location", false) &&
+      IsWin8OrLater()) {
     mProvider = new WindowsLocationProvider();
   }
 #endif
