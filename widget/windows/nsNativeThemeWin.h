b53d04c59c906cb1078104c399e169418fb31e01
K4sum1 <k4sum1@eclipse.cx>
2024-03-23 09:07:03 -0700

    Revert "Bug 1845623 - Make some form controls always non-native on Windows. r=mhowell,desktop-theme-reviewers,dao"
    This reverts commit e49eb28e3089c187f649895e2505917813dbb112.

--- widget/windows/nsNativeThemeWin.h
+++ widget/windows/nsNativeThemeWin.h
@@ -34,7 +34,6 @@ class nsNativeThemeWin : public Theme {
   // to avoid subtle sizing changes. The non-native theme can basically draw at
   // any size, so we prefer to have consistent sizing information.
   enum class NonNative { No, Always, BecauseColorMismatch };
-  static bool IsWidgetAlwaysNonNative(nsIFrame*, StyleAppearance);
   NonNative IsWidgetNonNative(nsIFrame*, StyleAppearance);
 
   // The nsITheme interface.
@@ -77,6 +76,8 @@ class nsNativeThemeWin : public Theme {
 
   bool ThemeSupportsWidget(nsPresContext* aPresContext, nsIFrame* aFrame,
                            StyleAppearance aAppearance) override;
+
+  bool WidgetIsContainer(StyleAppearance aAppearance) override;
 
   bool ThemeDrawsFocusForWidget(nsIFrame*, StyleAppearance) override;
 
