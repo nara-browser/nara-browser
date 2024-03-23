a15b1108d0543f5f735ebe8b4f14b15275db4173
K4sum1 <k4sum1@eclipse.cx>
2024-03-23 09:07:12 -0700

    Revert "Bug 1845636 - Allow browser chrome to enable the non-native theme. r=dshin"
    This reverts commit 55478c757e115baf6e4278d06edc11568520907a.

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -1747,7 +1747,6 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleUI {
   void TriggerImageLoads(mozilla::dom::Document&, const nsStyleUI*);
 
   mozilla::StyleInert mInert;
-  mozilla::StyleMozTheme mMozTheme;
 
  private:
   mozilla::StyleUserInput mUserInput;
