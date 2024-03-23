1553429e33e64c9bd7bda3396ed5b63d1a6f2e60
K4sum1 <k4sum1@eclipse.cx>
2024-03-23 10:44:22 -0700

    Revert "Bug 1843044 - Make windows titlebar system colors return our theme's colors for the default theme. r=mhowell"
    This reverts commit 58f73ae29a7559c351622ae8e68e58903debad5a.

--- widget/nsXPLookAndFeel.cpp
+++ widget/nsXPLookAndFeel.cpp
@@ -793,11 +793,6 @@ Maybe<nscolor> nsXPLookAndFeel::GenericDarkColor(ColorID aID) {
     case ColorID::MozComboboxtext:
     case ColorID::MozButtonhovertext:
     case ColorID::MozButtonactivetext:
-    case ColorID::MozHeaderbartext:
-    case ColorID::MozHeaderbarinactivetext:
-    case ColorID::Captiontext:
-    case ColorID::Inactivecaptiontext:  // TODO(emilio): Maybe make
-                                        // Inactivecaptiontext Graytext?
     case ColorID::MozColheadertext:
     case ColorID::MozColheaderhovertext:
     case ColorID::MozColheaderactivetext:
@@ -870,16 +865,6 @@ Maybe<nscolor> nsXPLookAndFeel::GenericDarkColor(ColorID aID) {
       // (browser.active_color.dark). See bug 1755564 for some analysis and
       // other options too.
       color = NS_RGB(0xff, 0x66, 0x66);
-      break;
-    case ColorID::Activeborder:
-    case ColorID::Inactiveborder:
-      color = NS_RGB(57, 57, 57);
-      break;
-    case ColorID::MozHeaderbar:
-    case ColorID::MozHeaderbarinactive:
-    case ColorID::Activecaption:
-    case ColorID::Inactivecaption:
-      color = NS_RGB(28, 27, 34);
       break;
     default:
       return Nothing();
