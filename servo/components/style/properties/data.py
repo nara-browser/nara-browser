89b54f2bf477363ff6a56445f9dab0ebd79bdcbe
K4sum1 <k4sum1@eclipse.cx>
2024-03-23 09:07:12 -0700

    Revert "Bug 1845636 - Allow browser chrome to enable the non-native theme. r=dshin"
    This reverts commit 55478c757e115baf6e4278d06edc11568520907a.

--- servo/components/style/properties/data.py
+++ servo/components/style/properties/data.py
@@ -594,7 +594,6 @@ class Longhand(Property):
                 "LineBreak",
                 "LineClamp",
                 "MasonryAutoFlow",
-                "ui::MozTheme",
                 "BoolInteger",
                 "text::MozControlCharacterVisibility",
                 "MathDepth",
