37b2c2dae672c89c0301e42f5604872cda3658a9
K4sum1 <k4sum1@eclipse.cx>
2024-03-28 19:51:32 -0700

    Revert "Bug 1868411 - Remove -moz-non-native-content-theme media feature. r=jwatt"
    This reverts commit 5985762903a53fcb8cf87e74df3f08e348bb661a.

--- servo/components/style/gecko/media_features.rs
+++ servo/components/style/gecko/media_features.rs
@@ -551,6 +551,10 @@ fn eval_moz_print_preview(context: &Context) -> bool {
     is_print_preview
 }
 
+fn eval_moz_non_native_content_theme(context: &Context) -> bool {
+    unsafe { bindings::Gecko_MediaFeatures_ShouldAvoidNativeTheme(context.device().document()) }
+}
+
 fn eval_moz_is_resource_document(context: &Context) -> bool {
     unsafe { bindings::Gecko_MediaFeatures_IsResourceDocument(context.device().document()) }
 }
@@ -948,6 +952,12 @@ pub static MEDIA_FEATURES: [QueryFeatureDescription; 59] = [
         atom!("-moz-print-preview"),
         AllowsRanges::No,
         Evaluator::BoolInteger(eval_moz_print_preview),
+        FeatureFlags::CHROME_AND_UA_ONLY,
+    ),
+    feature!(
+        atom!("-moz-non-native-content-theme"),
+        AllowsRanges::No,
+        Evaluator::BoolInteger(eval_moz_non_native_content_theme),
         FeatureFlags::CHROME_AND_UA_ONLY,
     ),
     feature!(
