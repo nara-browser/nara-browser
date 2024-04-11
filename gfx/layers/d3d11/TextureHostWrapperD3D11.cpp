4b6e5b11fac5cc5c689dfb62a6cb007eb54ab54a
K4sum1 <k4sum1@eclipse.cx>
2024-04-11 18:53:15 -0500

    Revert "Bug 1861605 - Use IDXGIResource1::CreateSharedHandle() instead of IDXGIResource::GetSharedHandle() r=gfx-reviewers,lsalzman"
    This reverts commit 7d06300567c2b66fb55323d8e3ace5603a8b1dff.

--- gfx/layers/d3d11/TextureHostWrapperD3D11.cpp
+++ gfx/layers/d3d11/TextureHostWrapperD3D11.cpp
@@ -254,7 +254,7 @@ RefPtr<TextureHost> TextureHostWrapperD3D11::CreateFromBufferTexture(
   auto colorSpace = ToColorSpace2(bufferTexture->GetYUVColorSpace());
 
   auto descD3D10 = SurfaceDescriptorD3D10(
-      nullptr, Some(id),
+      WindowsHandle(nullptr), Some(id),
       /* arrayIndex */ 0, gfx::SurfaceFormat::NV12, size, colorSpace,
       colorRange, /* hasKeyedMutex */ false, /* fenceInfo */ Nothing(),
       /* gpuProcessQueryId */ Nothing());
