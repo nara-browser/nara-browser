c2bb2e2e8316fb122d21b8b4efbafb3223e7c1fe
K4sum1 <k4sum1@eclipse.cx>
2024-05-12 17:38:05 -0500

    Revert "Bug 1362907 - Make select use nsHTMLButtonControlFrame for layout. r=jfkthame,dholbert"
    This reverts commit fb89acd3b51fa6efdd88840962e86c80aaf40be8.

--- layout/generic/FrameClasses.py
+++ layout/generic/FrameClasses.py
@@ -56,8 +56,13 @@ FRAME_CLASSES = [
     Frame("nsColorControlFrame", "ColorControl", REPLACED_WITH_BLOCK | LEAF),
     Frame("nsColumnSetFrame", "ColumnSet", COMMON),
     Frame("ColumnSetWrapperFrame", "ColumnSetWrapper", BLOCK | BFC),
-    Frame("nsComboboxControlFrame", "ComboboxControl", REPLACED_WITH_BLOCK | LEAF),
-    Frame("ComboboxLabelFrame", "Block", BLOCK),
+    Frame("nsComboboxControlFrame", "ComboboxControl", BLOCK | REPLACED_WITH_BLOCK),
+    # FIXME(emilio, bug 1362907): Revisit these after that bug, this is the
+    # only frame that has ReplacedContainsBlock but not Replaced, which is
+    # sketchy.
+    Frame(
+        "nsComboboxDisplayFrame", "ComboboxDisplay", REPLACED_WITH_BLOCK - {"Replaced"}
+    ),
     Frame("nsContinuingTextFrame", "Text", TEXT),
     Frame("nsDateTimeControlFrame", "DateTimeControl", REPLACED_WITH_BLOCK),
     Frame("nsFieldSetFrame", "FieldSet", BLOCK),
