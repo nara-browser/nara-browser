1cdbb0f4103b2be11e98f0f5831cd844009cc9fb
K4sum1 <k4sum1@eclipse.cx>
2024-06-15 08:27:58 -0500

    I̴̧͎͂ ̵̢̈́d̷̼͓͐i̷̻͚̓d̷̛͍͒ ̴̼̆i̸̛̠͔͊t̴̙̓͝ ̶̘͐f̵̺̹̊ȯ̶̲r̶͙̂ ̴̠͗͝y̸̥͌o̴͔̐̃u̵̫͘ ̴̰͐̒͜T̶̞͇͋ő̷̫͓b̵̦̦̆ĭ̸̜̾ṉ̸͒
    [ERROR]   You have perished under maximum extermination.
    [ERROR]   Closing link: (ambassador@104.244.209.39) [Z-lined: Furry Porn Firefox Builds are forbidden.]
           ===   Connection to irc://irc.binaryoutcast.com/ (irc://irc.binaryoutcast.com/) closed. [Help]

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -168,6 +168,8 @@ static const RedirEntry kRedirMap[] = {
          nsIAboutModule::URI_MUST_LOAD_IN_CHILD | nsIAboutModule::ALLOW_SCRIPT |
          nsIAboutModule::URI_CAN_LOAD_IN_PRIVILEGEDABOUT_PROCESS |
          nsIAboutModule::HIDE_FROM_ABOUTABOUT},
+    {"yiff", "chrome://browser/content/yiff.webm",
+     nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT},
 };
 
 static nsAutoCString GetAboutModuleName(nsIURI* aURI) {
