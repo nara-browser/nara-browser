dd09be5aa9dd8781090603b8a304139d49f61b5f
K4sum1 <k4sum1@eclipse.cx>
2024-06-15 11:29:04 -0500

    finally commiting these

--- browser/branding/r3dfox/configure.sh
+++ browser/branding/r3dfox/configure.sh
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_NAME=r3dfox
+MOZ_APP_NAME="Eclipse r3dfox"
 
 MOZ_APP_BASENAME="r3dfox"
 
