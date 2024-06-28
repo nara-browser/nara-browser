62e77e1dbfaf3d85c3ae55bae5570c8f8963180b
K4sum1 <k4sum1@eclipse.cx>
2024-06-28 13:46:52 -0500

    r3dfox customizations

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -685,6 +685,14 @@ pref("toolkit.telemetry.dap.logLevel", "Warn");
 // Extra logging for AsyncShutdown barriers and phases
 pref("toolkit.asyncshutdown.log", false);
 
+// r3dfox preferences
+// Red tint on private window
+pref("r3dfox.customizations.enabled", true);
+// Aero fog configurations
+pref("r3dfox.less.aero.fog", true);
+pref("r3dfox.aero.fog", true);
+pref("r3dfox.force.transparency", false);
+
 // Enable JS dump() function.
 // IMPORTANT: These prefs must be here even though they're also defined in
 // StaticPrefList.yaml. They are required because MOZILLA_OFFICIAL is false in
