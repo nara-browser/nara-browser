c8117ca685a72a66219afeccd1841a464f8d47c2
K4sum1 <k4sum1@eclipse.cx>
2024-07-01 12:51:02 -0500

    Block VM hardware acceleration on VirtualBox for being buggy

--- widget/windows/GfxInfo.cpp
+++ widget/windows/GfxInfo.cpp
@@ -2041,9 +2041,6 @@ nsresult GfxInfo::GetFeatureStatusImpl(
         !adapterVendorID.Equals(
             GfxDriverInfo::GetDeviceVendor(DeviceVendor::VMWare),
             nsCaseInsensitiveStringComparator) &&
-        !adapterVendorID.Equals(
-            GfxDriverInfo::GetDeviceVendor(DeviceVendor::VirtualBox),
-            nsCaseInsensitiveStringComparator) &&
         // FIXME - these special hex values are currently used in xpcshell tests
         // introduced by bug 625160 patch 8/8. Maybe these tests need to be
         // adjusted now that we're only whitelisting intel/ati/nvidia.
@@ -2051,7 +2048,11 @@ nsresult GfxInfo::GetFeatureStatusImpl(
         !adapterVendorID.LowerCaseEqualsLiteral("0xdcba") &&
         !adapterVendorID.LowerCaseEqualsLiteral("0xabab") &&
         !adapterVendorID.LowerCaseEqualsLiteral("0xdcdc")) {
-      if (adapterVendorID.Equals(GfxDriverInfo::GetDeviceVendor(
+      if (adapterVendorID.Equals(
+              GfxDriverInfo::GetDeviceVendor(DeviceVendor::VirtualBox),
+              nsCaseInsensitiveStringComparator)) {
+        aFailureId = "FEATURE_FAILURE_VM_VENDOR";
+      } else if (adapterVendorID.Equals(GfxDriverInfo::GetDeviceVendor(
                                             DeviceVendor::MicrosoftBasic),
                                         nsCaseInsensitiveStringComparator)) {
         aFailureId = "FEATURE_FAILURE_MICROSOFT_BASIC_VENDOR";
