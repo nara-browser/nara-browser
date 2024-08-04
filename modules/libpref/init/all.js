f522a5df7160d3d054b2533734747bc74fd50a9f
K4sum1 <k4sum1@eclipse.cx>
2024-08-04 12:20:32 -0500

    Add useragent overrides for compatibility

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -78,6 +78,9 @@ pref("general.useragent.compatMode.firefox", false);
 
 // Enable site specific overrides to fix shitty websites
 pref("general.useragent.site_specific_overrides", true);
+pref("general.useragent.override.discord.com", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0");
+pref("general.useragent.override.snapchat.com", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
+pref("general.useragent.override.whatsapp.com", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0");
 
 pref("general.config.obscure_value", 13); // for MCD .cfg files
 
