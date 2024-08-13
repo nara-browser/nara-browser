38680c06d858b6a13780f271f9b13b54e235deff
K4sum1 <k4sum1@eclipse.cx>
2024-08-13 10:20:07 -0500

    Revert "Bug 1824053 - [4/4] cloak and clear new window before showing it  r=win-reviewers,handyman"
    This reverts commit 799ee37eab59712cd4d84a397988e59ed52770ae.

--- mozglue/misc/PreXULSkeletonUI.cpp
+++ mozglue/misc/PreXULSkeletonUI.cpp
@@ -7,7 +7,6 @@
 #include "PreXULSkeletonUI.h"
 
 #include <algorithm>
-#include <dwmapi.h>
 #include <math.h>
 #include <limits.h>
 #include <cmath>
@@ -148,8 +147,6 @@ MOZ_DECL_IMPORTED_WIN32_FN(GetMonitorInfoW);
 MOZ_DECL_IMPORTED_WIN32_FN(SetWindowLongPtrW);
 MOZ_DECL_IMPORTED_WIN32_FN(StretchDIBits);
 MOZ_DECL_IMPORTED_WIN32_FN(CreateSolidBrush);
-MOZ_DECL_IMPORTED_WIN32_FN(DwmGetWindowAttribute);
-MOZ_DECL_IMPORTED_WIN32_FN(DwmSetWindowAttribute);
 #undef MOZ_DECL_IMPORTED_WIN32_FN
 
 static int sWindowWidth;
@@ -657,17 +654,6 @@ bool RasterizeAnimatedRect(const ColorRect& colorRect,
   return true;
 }
 
-bool FillRectWithColor(HDC hdc, LPCRECT rect, uint32_t mozColor) {
-  HBRUSH brush = sCreateSolidBrush(RGB((mozColor & 0xff0000) >> 16,
-                                       (mozColor & 0x00ff00) >> 8,
-                                       (mozColor & 0x0000ff) >> 0));
-  int fillRectResult = sFillRect(hdc, rect, brush);
-
-  sDeleteObject(brush);
-
-  return !!fillRectResult;
-}
-
 Result<Ok, PreXULSkeletonUIError> DrawSkeletonUI(
     HWND hWnd, CSSPixelSpan urlbarCSSSpan, CSSPixelSpan searchbarCSSSpan,
     Vector<CSSPixelSpan>& springs, const ThemeColors& currentTheme,
@@ -1078,10 +1064,15 @@ Result<Ok, PreXULSkeletonUIError> DrawSkeletonUI(
 
   // Then, we just fill the rest with FillRect
   RECT rect = {0, sTotalChromeHeight, sWindowWidth, sWindowHeight};
-  bool const fillRectOk =
-      FillRectWithColor(hdc, &rect, currentTheme.backgroundColor);
+  HBRUSH brush =
+      sCreateSolidBrush(RGB((currentTheme.backgroundColor & 0xff0000) >> 16,
+                            (currentTheme.backgroundColor & 0x00ff00) >> 8,
+                            (currentTheme.backgroundColor & 0x0000ff) >> 0));
+  int fillRectResult = sFillRect(hdc, &rect, brush);
 
-  if (!fillRectOk) {
+  sDeleteObject(brush);
+
+  if (fillRectResult == 0) {
     return Err(PreXULSkeletonUIError::FailedFillingBottomRect);
   }
 
@@ -1381,9 +1372,8 @@ Result<HKEY, PreXULSkeletonUIError> OpenPreXULSkeletonUIRegKey() {
 Result<Ok, PreXULSkeletonUIError> LoadGdi32AndUser32Procedures() {
   HMODULE user32Dll = ::LoadLibraryW(L"user32");
   HMODULE gdi32Dll = ::LoadLibraryW(L"gdi32");
-  HMODULE dwmapiDll = ::LoadLibraryW(L"dwmapi.dll");
 
-  if (!user32Dll || !gdi32Dll || !dwmapiDll) {
+  if (!user32Dll || !gdi32Dll) {
     return Err(PreXULSkeletonUIError::FailedLoadingDynamicProcs);
   }
 
@@ -1419,8 +1409,6 @@ Result<Ok, PreXULSkeletonUIError> LoadGdi32AndUser32Procedures() {
   MOZ_LOAD_OR_FAIL(user32Dll, ShowWindow);
   MOZ_LOAD_OR_FAIL(user32Dll, SetWindowPos);
   MOZ_LOAD_OR_FAIL(user32Dll, GetWindowDC);
-  MOZ_LOAD_OR_FAIL(user32Dll, GetWindowRect);
-  MOZ_LOAD_OR_FAIL(user32Dll, MapWindowPoints);
   MOZ_LOAD_OR_FAIL(user32Dll, FillRect);
   MOZ_LOAD_OR_FAIL(user32Dll, ReleaseDC);
   MOZ_LOAD_OR_FAIL(user32Dll, LoadIconW);
@@ -1431,8 +1419,6 @@ Result<Ok, PreXULSkeletonUIError> LoadGdi32AndUser32Procedures() {
   MOZ_LOAD_OR_FAIL(gdi32Dll, StretchDIBits);
   MOZ_LOAD_OR_FAIL(gdi32Dll, CreateSolidBrush);
   MOZ_LOAD_OR_FAIL(gdi32Dll, DeleteObject);
-  MOZ_LOAD_OR_FAIL(dwmapiDll, DwmGetWindowAttribute);
-  MOZ_LOAD_OR_FAIL(dwmapiDll, DwmSetWindowAttribute);
 
 #undef MOZ_LOAD_OR_FAIL
 
@@ -1932,44 +1918,7 @@ static Result<Ok, PreXULSkeletonUIError> CreateAndStorePreXULSkeletonUIImpl(
     return Err(PreXULSkeletonUIError::CreateWindowFailed);
   }
 
-  // DWM displays garbage immediately on Show(), and that garbage is usually
-  // mostly #FFFFFF. To avoid a bright flash when the window is first created,
-  // cloak the window while showing it, and fill it with the appropriate
-  // background color before uncloaking it.
-  if (sDwmGetWindowAttribute != nullptr) {
-    constexpr static auto const CloakWindow = [](HWND hwnd, BOOL state) {
-      sDwmSetWindowAttribute(sPreXULSkeletonUIWindow, DWMWA_CLOAK, &state,
-                             sizeof(state));
-    };
-    // Equivalent to ::OffsetRect, with no dynamic-symbol resolution needed.
-    constexpr static auto const OffsetRect = [](LPRECT rect, int dx, int dy) {
-      rect->left += dx;
-      rect->top += dy;
-      rect->right += dx;
-      rect->bottom += dy;
-    };
-
-    CloakWindow(sPreXULSkeletonUIWindow, TRUE);
-    auto const _uncloak =
-        MakeScopeExit([&]() { CloakWindow(sPreXULSkeletonUIWindow, FALSE); });
-    sShowWindow(sPreXULSkeletonUIWindow, showCmd);
-
-    HDC hdc = sGetWindowDC(sPreXULSkeletonUIWindow);
-    if (!hdc) {
-      return Err(PreXULSkeletonUIError::FailedGettingDC);
-    }
-    auto const _cleanupDC =
-        MakeScopeExit([&] { sReleaseDC(sPreXULSkeletonUIWindow, hdc); });
-
-    // This should match the related code in nsWindow::Show.
-    RECT rect;
-    sGetWindowRect(sPreXULSkeletonUIWindow, &rect);  // includes non-client area
-    // screen-to-client (handling RTL if necessary)
-    sMapWindowPoints(HWND_DESKTOP, sPreXULSkeletonUIWindow, (LPPOINT)&rect, 2);
-    // client-to-window (no RTL handling needed)
-    OffsetRect(&rect, -rect.left, -rect.top);
-    FillRectWithColor(hdc, &rect, currentTheme.backgroundColor);
-  }
+  sShowWindow(sPreXULSkeletonUIWindow, showCmd);
 
   sDpi = sGetDpiForWindow(sPreXULSkeletonUIWindow);
   sNonClientHorizontalMargins =
